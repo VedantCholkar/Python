462db7878852ac1508b73867c4c583fe8038d317
Vedant Cholkar <95518138+VedantCholkar@users.noreply.github.com>
2022-02-21 17:25:57 +0530

    Update guess helper

--- Number Guesser (User)/Number Guesser.py
+++ Number Guesser (User)/Number Guesser.py
@@ -3,14 +3,16 @@ import random
 lower_limit = int(input("Choose your lower limit: "))
 higher_limit = int(input("Choose your higher limit: "))
 random_int = random.randint(lower_limit, higher_limit)
+score = 0
 guess = 0
 
 while guess != random_int:
     guess = int(input("Guess the number: "))
-    if guess < random_int:
-        print("Too low, try again")
+    score += 1
+    if guess == random_int:
+        print('\n')
+        print(f"You win! 😃. Your score is {score}")
     elif guess > random_int:
         print("Too high, try again")
-    else:
-        print('\n')
-        print("You win! 😃")
+    elif guess < random_int:
+        print("Too low, try again")
